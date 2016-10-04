72b736318b0f923056ac8e02226f4df067a490c0
Pedro Madrigal <dnaseiseq@gmail.com>
2016-10-04 17:06:42 +0100

    Create heatmapfCCAC.R

---
+++ R/heatmapfCCAC.R
@@ -0,0 +1,32 @@
+heatmapfCCAC <- function(fc){
+  
+  M <- c() #store sample names
+  for (i in 1:length(fc$samples) ){
+    M<- c(M, strsplit( as.character(fc$samples[i]), split="_vs_")[[1]] )
+  }
+  M <- unique(M)
+  
+#plot heatmap
+#if (length(tf) == 0){
+#  M <- unique( as.vector(co) )
+  Fv <- matrix(NA, nrow=length(M), ncol=length(M))
+  for (i in 1:length(M)){
+    for (j in 1:length(M)){
+      o1 <- which(as.character(fc$samples) == paste(M[i], M[j], sep="_vs_" ) )
+      o2 <- which(as.character(fc$samples) == paste(M[j], M[i], sep="_vs_" ) )
+      if (length(o1)==1){ Fv[i,j] <- fc$F[o1] }
+      if (length(o2)==1){ Fv[i,j] <- fc$F[o2] }
+      if (length(o1)==0 & length(o2)==0) {Fv[j,i] <- 100 }
+    }
+  }
+  colnames(Fv) <- M
+  rownames(Fv) <- M
+  #library(ComplexHeatmap)
+
+  Heatmap(Fv, name='F (%)')
+
+
+
+
+
+}
