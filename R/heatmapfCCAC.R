feea6970866958d94ad44d9fd5240f34a9c244e8
Pedro Madrigal <dnaseiseq@gmail.com>
2016-10-04 17:23:42 +0100

    Update heatmapfCCAC.R

--- R/heatmapfCCAC.R
+++ R/heatmapfCCAC.R
@@ -7,8 +7,6 @@ heatmapfCCAC <- function(fc){
   M <- unique(M)
   
 #plot heatmap
-#if (length(tf) == 0){
-#  M <- unique( as.vector(co) )
   Fv <- matrix(NA, nrow=length(M), ncol=length(M))
   for (i in 1:length(M)){
     for (j in 1:length(M)){
@@ -21,11 +19,9 @@ heatmapfCCAC <- function(fc){
   }
   colnames(Fv) <- M
   rownames(Fv) <- M
-  #library(ComplexHeatmap)
+
 
   Heatmap(Fv, name='F (%)')
-
-
 
 
 
