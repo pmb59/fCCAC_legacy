e0f82337f3bf74a08f7f656df20a303bb295cba2
Pedro Madrigal <dnaseiseq@gmail.com>
2016-10-15 18:59:07 +0100

    Update multiplot.R

--- R/multiplot.R
+++ R/multiplot.R
@@ -14,7 +14,6 @@
 #
 
 multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
- # library(grid)
 
   # Make a list from the ... arguments and plotlist
   plots <- c(list(...), plotlist)
@@ -26,11 +25,10 @@ multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
     # Make the panel
     # ncol: Number of columns of plots
     # nrow: Number of rows needed, calculated from # of cols
-    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
-                    ncol = cols, nrow = ceiling(numPlots/cols))
+    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)), ncol = cols, nrow = ceiling(numPlots/cols))
   }
 
- if (numPlots==1) {
+  if (numPlots==1) {
     print(plots[[1]])
 
   } else {
@@ -42,9 +40,8 @@ multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
     for (i in 1:numPlots) {
       # Get the i,j matrix positions of the regions that contain this subplot
       matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
-
-      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
-                                      layout.pos.col = matchidx$col))
+      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row, layout.pos.col = matchidx$col))
     }
   }
 }
+
