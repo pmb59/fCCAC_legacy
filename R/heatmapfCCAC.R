9d4bd310cd523f971bac4205736020641df6dc08
Pedro Madrigal <dnaseiseq@gmail.com>
2016-10-16 13:07:34 +0100

    Update heatmapfCCAC.R

--- R/heatmapfCCAC.R
+++ R/heatmapfCCAC.R
@@ -1,10 +1,20 @@
 heatmapfCCAC <- function(fc){
   
-  M <- c() #store sample names
+#  M <- c() #store sample names
+#  for (i in seq(from=1, to=length(fc$samples), by=1)    ){
+#    M <- c(M, strsplit( as.character(fc$samples[i]), split="_vs_")[[1]] )
+#  }
+#  M <- unique(M)
+   
+  M <- rep("name", nrow(fc)*2)  #store sample names
+  k <- 0
   for (i in seq(from=1, to=length(fc$samples), by=1)    ){
-    M<- c(M, strsplit( as.character(fc$samples[i]), split="_vs_")[[1]] )
+    temp_names <- strsplit( as.character(fc$samples[i]), split="_vs_")[[1]] 
+    k<- k +1 ; M[k] <- temp_names[1]
+    k<- k +1 ; M[k] <- temp_names[2]
   }
   M <- unique(M)
+  rm(k)
   
   #plot heatmap
   Fv <- matrix(NA, nrow=length(M), ncol=length(M))
